60ce2ef6af50c441558c2a6b81844d86354cf6af
guilhermedurbano <guilhermedurbano@gmail.com>
2023-04-18 16:31:09 +0100

    implementacao de subcomandos de campo harmonico
    Signed-off-by: guilhermedurbano <guilhermedurbano@gmail.com>

--- musical_notes/cli.py
+++ musical_notes/cli.py
@@ -3,6 +3,7 @@ from rich.table import Table
 from typer import Argument, Typer
 
 from musical_notes.escalas import escala as _escala
+from musical_notes.campo_harmonico import campo_harmonico as _campo_harmonico
 from musical_notes.acordes import acorde as _acorde
 
 console = Console()
@@ -33,4 +34,20 @@ def acorde(cifra: str = Argument('C', help='Cifra do acorde')):
         table.add_column(grau)
 
     table.add_row(*notas)
+    console.print(table)
+
+
+@app.command()
+def campo_harmonico(tonica: str = Argument('c',
+                                           help='Tônica do campo harmonico'
+                                           ),
+                    tonalidade: str = Argument('maior',
+                                               help='Tonalidade do campo')):
+    table = Table()
+    acordes, graus = _campo_harmonico(tonica, tonalidade).values()
+
+    for grau in graus:
+        table.add_column(grau)
+
+    table.add_row(*acordes)
     console.print(table)
